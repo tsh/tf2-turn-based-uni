3288d2c464e321e7ca76bbf5ec1429b24732e3df
tsh <dr.tallin@gmail.com>
2016-01-06 20:12:45 +0200

    tiles

--- client/core/src/io/gameclient/game/Tf2Client.java
+++ client/core/src/io/gameclient/game/Tf2Client.java
@@ -1,27 +1,65 @@
 package io.gameclient.game;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.graphics.Color;
+
+
+
 
 public class Tf2Client extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+    ShapeRenderer shapeRenderer;
+    int[][] map;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture(Gdx.files.internal("badlogic.jpg"));
-	}
+        map = new int[][]{
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 55, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        shapeRenderer = new ShapeRenderer();
+    }
 
 	@Override
-	public void render () {
+	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+        drawMap(batch);
 		batch.end();
+
+
 	}
+
+
+    public void drawMap(SpriteBatch batch){
+        for (int i = 0; i < map.length; i++){
+            for (int j = 0; j < map[i].length; j++){
+                shapeRenderer.begin(ShapeType.Filled);
+                if (map[i][j] == 0) {
+                    shapeRenderer.setColor(Color.BLACK);
+                } else{
+                    shapeRenderer.setColor(Color.FOREST);
+                }
+                shapeRenderer.rect(50*j, 50*i, 50, 50);
+                shapeRenderer.end();
+            }
+        }
+    }
+
+
 }
