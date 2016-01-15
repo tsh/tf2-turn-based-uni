e35e0e207bcaa80559f67218f356b4e08c6cf1c5
tsh <dr.tallin@gmail.com>
2016-01-16 00:35:52 +0200

    libgdx menu background image

--- client/core/src/io/gameclient/game/MainMenuScreen.java
+++ client/core/src/io/gameclient/game/MainMenuScreen.java
@@ -3,13 +3,35 @@ package io.gameclient.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class MainMenuScreen implements Screen {
     final Tf2Client game;
     OrthographicCamera camera;
+
     Music backgroundMusic;
+    Texture backgroundTexture;
+
+    private Stage stage;
+    private Table table;
+    Skin skin;
+
+    Actor root;
 
     public MainMenuScreen(final Tf2Client gam) {
         game = gam;
@@ -22,29 +44,61 @@ public class MainMenuScreen implements Screen {
         backgroundMusic.setLooping(true);
         backgroundMusic.play();
 
+        this.backgroundTexture = new Texture(Gdx.files.internal("ui/2fort_800px.png"));
+
+        stage = new Stage(new StretchViewport(640, 480));
+        Gdx.input.setInputProcessor(stage);
+
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin = new Skin();
+        skin.add("white", new Texture(pixmap));
+        skin.add("default", new BitmapFont());
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        table.setDebug(true);
+
+        final TextButton button = new TextButton("Click me!", skin);
+        table.add(button);
+
+        button.addListener(new ChangeListener() {
+            public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+                System.out.println("Clicked! Is checked: " + button.isChecked());
+                button.setText("Good job!");
+            }
+        });
+        table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.getBatch().begin();
+        stage.getBatch().draw(this.backgroundTexture, 0, 0);
+        stage.getBatch().end();
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-
-        game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
-        game.batch.end();
-
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new GameScreen(game));
-            dispose();
-        }
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+//
+//        if (Gdx.input.isTouched()) {
+//            game.setScreen(new GameScreen(game));
+//            dispose();
+//        }
     }
 
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -65,6 +119,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
+        stage.dispose();
         backgroundMusic.dispose();
     }
 }
